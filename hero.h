e2cd8ffb00abd20c3ea14f01e623a17774808279
mateuszpawlowski1g <mati1076@gmail.com>
2020-05-18 17:10:06 +0200

    Update hero.h

--- hero.h
+++ hero.h
@@ -1,7 +1,14 @@
 Class
 {
  int HP_level;
-  int Max_Hp;
-  double speed;
-  
+ int Max_Hp;
+ double speed;
+ double pos_x;
+ double pos_y;
+ double DMG;
+ double atack_ratio;
+ public:
+ void move();
+ virtual void atack();
+ virtual void get_hit();
 }
